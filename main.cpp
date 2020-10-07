9018d19b86a5b9a98742935825b0b7a4f2e0cc73
Michese <Michese48@mail.ru>
2020-10-07 16:05:44 +0300

    fifth commit lesson-3

--- main.cpp
+++ main.cpp
@@ -1,42 +1,116 @@
-#include "functions/sine.h"
+#include <iostream>
+#include <vector>
+#include <cmath>
 #include <random>
 #include <ctime>
-using namespace functions;
-using namespace functions_base;
+
+using namespace std;
+class BaseFunction
+{
+protected:
+    vector<double> a;
+    int countParams;
+    double x;
+
+public:
+    double getNumber(char symbolOfNumber)
+    {
+        double result = 0.0;
+        cout << "Введите значение параметра " << symbolOfNumber << ": ";
+        try
+        {
+            cin >> result;
+        }
+        catch (...)
+        {
+            cout << "Не корректное значение параметра " << symbolOfNumber << '!' << endl;
+            result = getNumber(symbolOfNumber);
+        }
+
+        return result;
+    }
+    void set()
+    {
+        char symbolParam = 'a';
+        for (int count = 0; count < countParams; count++, symbolParam++)
+        {
+            a.push_back(getNumber(symbolParam));
+        }
+        x = getNumber('x');
+    }
+    void set(vector<double> params, double x)
+    {
+        if (this->countParams > params.size())
+        {
+            throw "Параметров не достаточно!";
+        }
+        for (int count = 0; count < countParams; count++)
+        {
+            this->a.push_back(params[count]);
+        }
+        this->x = x;
+    }
+    virtual int getResult() = 0;
+    virtual void print() = 0;
+};
+
+class Exponent : public BaseFunction
+{
+public:
+    Exponent()
+    {
+        countParams = 2;
+    }
+    int getResult() override
+    {
+        return this->a[0] * exp(this->a[1] * this->x);
+    }
+    void print() override
+    {
+        cout << this->a[0] << " * exp^( " << this->a[1] << " * " << this->x << " ) = " << this->getResult() << endl;
+    }
+};
+
+class Sine : public BaseFunction
+{
+public:
+    Sine()
+    {
+        countParams = 2;
+    }
+    int getResult()
+    {
+        return this->a[0] * sin(this->x * this->a[1]);
+    }
+    void print() override
+    {
+        cout << this->a[0] << " * sin( " << this->a[1] << " * " << this->x << " ) = " << this->getResult() << endl;
+    }
+};
 
 int main()
 {
-    srand(time(NULL));
-    int size = rand() % 10 + 1;
-    int a, b, x;
-    BaseFunction **array = new BaseFunction *[size];
-    for (int row = 0; row < size; row++)
+    setlocale(LC_ALL, "Russian");
+    int size = 0;
+    bool flag = true;
+    while (flag)
     {
-        if (rand() % 2 == 1)
+        cout << "Введите размерность вектора: ";
+        try
         {
-            array[row] = new Sine();
+            cin >> size;
+            flag = false;
         }
-        else
+        catch (...)
         {
-            array[row] = new Exponent();
+            cout << "Не корректная размерность вектора!" << endl;
         }
     }
+    cout << "Введите число соответствующее номеру нужной Вам функции:" << endl;
 
-    for (int row = 0; row < size; row++)
-    {
-        a = rand() % 20 + 1;
-        b = rand() % 5;
-        x = rand() % 10;
-        array[row]->setParams(a, b, x);
-        cout << "a = " << a << ";\nb = " << b << ";\nx = " << x << ';' << endl;
-        array[row]->print();
-    }
+    BaseFunction **vector = new BaseFunction *[size];
+    flag = true;
+    while ()
 
-    for (int row = 0; row < size; row++)
-    {
-        delete[] array[row];
-    }
-    delete[] array;
-
-    return 0;
+        return 0;
 }
